d3f3d992ef6d28e2d94f0de0c723f9e89b196d1d
sFisherE <397381741@qq.com>
2013-12-19 22:57:13 +0800

    some ui effect

---
+++ Project/Assets/UniUIEffect/Scripts/TouchTrail/TouchTrail.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///   touch trail,based on MeleeWeaponTrail
+/// </summary>
+class TouchTrail : MonoBehaviour
+{
+    public Transform trans;
+    public Camera camera;
+
+    Vector3 mPrePosition;
+    void Update()
+    {
+        if (enabled)
+        {
+            if (mPrePosition == Vector3.zero)
+            {
+                mPrePosition = trans.localPosition;//init the value
+                return;
+            }
+
+            Vector3 curPosition = TransformUtility.GetRelativeMousePosition(camera, trans.parent);
+            trans.localPosition = curPosition;
+
+            float theDistanceSqr = (mPrePosition - curPosition).sqrMagnitude;
+            if (theDistanceSqr > 0.1f)
+            {
+                trans.LookAt(transform.TransformPoint(curPosition - mPrePosition));
+                trans.localEulerAngles = Vector3.zero;//reset the rotate angle
+
+                Vector3 dir = curPosition - mPrePosition;
+                float angle = Vector2.Angle(dir, new Vector2(1, 0));
+                if (dir.y < 0)
+                    angle = -angle;
+
+                trans.Rotate(new Vector3(0, 0, angle));
+
+                mPrePosition = curPosition;
+            }
+        }
+    }
+
+}
---
+++ Project/Assets/UniUIEffect/Scripts/TransformUtility.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransformUtility
+{
+
+    /// <summary>
+    ///   get relative mouse position under parent node
+    ///   the camera must be orthometric
+    /// </summary>
+    static public Vector3 GetRelativeMousePosition(Camera camera, Transform parent)
+    {
+        Vector3 v = camera.ScreenToWorldPoint(Input.mousePosition);//screen position to world position
+        if (parent!=null)
+            v = parent.InverseTransformPoint(v);//world position to local position
+
+        v.z = 0;//flat z 
+        return v;
+    }
+
+
+
+}
